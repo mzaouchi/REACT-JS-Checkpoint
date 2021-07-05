7370f63cf891b316cd141b90b80f37816f060eb6
mzaouchi <mzaouchi@gmail.com>
2021-07-05 17:05:09 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+import FormLabel from 'react-bootstrap/FormLabel';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+              <div className="ContB">
+                <Form>
+
+                  <Form.Group>
+                  <Form.Label>Nom :</Form.Label>
+                  <Form.Control type="text" placeholder="Votre nom"></Form.Control>
+                  </Form.Group>
+
+                  <Form.Group>
+                  <Form.Label>Prénom :</Form.Label>
+                  <Form.Control type="text" placeholder="Votre prénom"></Form.Control>
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label>Sexe :</Form.Label>
+                    <Form.Control as="select">
+                      <option>Masculin</option>
+                      <option>Féminin</option>      
+                    </Form.Control>
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label>Email :</Form.Label>
+                    <Form.Control type="email" placeholder="exemple@email.com"></Form.Control>
+                  </Form.Group>
+
+                  <Form.Group>
+                    <FormLabel>Mot de passe :</FormLabel>
+                    <Form.Control type="password"></Form.Control>
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Check type="checkbox" label="J'accepte les conditions" />
+                  </Form.Group>
+
+                  <Button type="submit" variant="primary">Envoyer</Button>
+                </Form>       
+
+                </div>   
+             
   );
 }
 
